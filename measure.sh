1e429e1f9b3fb85b84ebb94a9dc0c38e39aec412
Heidi-ann <hh360@cam.ac.uk>
2015-08-28 13:45:22 +0100

    removing tilda

--- measure.sh
+++ measure.sh
@@ -2,7 +2,10 @@
 
 SERVERS=(1 2 3 4 5)
 
-LOG_FILE="~/hping_log.csv"
+DIR="/home/azureuser/azure-measurements/"
+
+LOG_FILE=$DIR"hping_log.csv"
+TEMP_FILE=$DIR"output.temp"
 touch $LOG_FILE
 
 while true
@@ -11,8 +14,8 @@ do
 	do
 		SNAME="measurement-server-"$NAME".cloudapp.net"
 		echo "pinging $SNAME"
-		hping3 $SNAME -c 3 -p 80 -S 2> ~/output.temp
-		RTT=`cat output.temp | grep round-trip | awk -F '/' '{print $4}'`
+		hping3 $SNAME -c 3 -p 80 -S 2> $TEMP_FILE
+		RTT=`cat $TEMP_FILE | grep round-trip | awk -F '/' '{print $4}'`
 		TIME=`date`
 		echo "$TIME, $NAME, $RTT" >> $LOG_FILE 
 	done
