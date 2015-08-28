8df4cd5ffc939be0e45ef4f6dfdb5b11f72a2ddd
Heidi-ann <hh360@cam.ac.uk>
2015-08-28 13:23:14 +0100

    making paths absolute

--- measure.sh
+++ measure.sh
@@ -2,7 +2,7 @@
 
 SERVERS=(1 2 3 4 5)
 
-LOG_FILE="hping_log.csv"
+LOG_FILE="~/hping_log.csv"
 touch $LOG_FILE
 
 while true
@@ -11,7 +11,7 @@ do
 	do
 		SNAME="measurement-server-"$NAME".cloudapp.net"
 		echo "pinging $SNAME"
-		hping3 $SNAME -c 3 -p 80 -S 2> output.temp
+		hping3 $SNAME -c 3 -p 80 -S 2> ~/output.temp
 		RTT=`cat output.temp | grep round-trip | awk -F '/' '{print $4}'`
 		TIME=`date`
 		echo "$TIME, $NAME, $RTT" >> $LOG_FILE 
