1799dddda188b233d57b11fe2244a9062cef7bdf
Heidi-ann <hh360@cam.ac.uk>
2015-09-02 12:26:19 +0100

    adding temp file history

--- measure.sh
+++ measure.sh
@@ -5,8 +5,13 @@ SERVERS=(1 2 3 4 5)
 DIR="/home/azureuser/azure-measurements/"
 
 LOG_FILE=$DIR"hping_log.csv"
+
 TEMP_FILE=$DIR"output.temp"
+HIST_FILE=$DIR"output.hist"
+
 touch $LOG_FILE
+touch $TEMP_FILE
+touch $HIST_FILE
 
 while true
 do
@@ -15,6 +20,7 @@ do
 		SNAME="measurement-server-"$NAME".cloudapp.net"
 		echo "pinging $SNAME"
 		hping3 $SNAME -c 3 -p 80 -S 2> $TEMP_FILE
+		echo $TEMP_FILE >> $HIST_FILE
 		RTT=`cat $TEMP_FILE | grep round-trip | awk -F '/' '{print $4}'`
 		if [ "$RTT" != "" ]
 		then
