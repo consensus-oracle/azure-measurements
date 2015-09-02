410bec8e809c0f42b7b2b594980fc1f34652b941
Heidi-ann <hh360@cam.ac.uk>
2015-09-02 11:55:49 +0100

    fixed

--- measure.sh
+++ measure.sh
@@ -2,7 +2,7 @@
 
 SERVERS=(1 2 3 4 5)
 
-DIR="/home/azureuser/azure-measurements/"
+DIR="~/azure-measurements/"
 
 LOG_FILE=$DIR"hping_log.csv"
 TEMP_FILE=$DIR"output.temp"
@@ -16,7 +16,7 @@ do
 		echo "pinging $SNAME"
 		hping3 $SNAME -c 3 -p 80 -S 2> $TEMP_FILE
 		RTT=`cat $TEMP_FILE | grep round-trip | awk -F '/' '{print $4}'`
-		if ["$RTT" -ne ""];
+		if ["$RTT" -ne ""]
 		then
 			TIME=`date`
 			echo "$TIME, $NAME, $RTT" >> $LOG_FILE 
