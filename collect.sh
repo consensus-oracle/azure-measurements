909557c74144108b4abb848f4824741eb266547c
Heidi-ann <hh360@cam.ac.uk>
2015-09-04 10:06:06 +0100

    agregating results into single file

--- collect.sh
+++ collect.sh
@@ -2,9 +2,12 @@
 
 source common.sh 
 FILENAME="hping_log.csv"
+touch latency-results.csv
 
 for NAME in ${SERVERS[*]}
 do
 	ADDR="measurement-server-"$NAME".cloudapp.net"
-	scp -i ~/.ssh/azure_key $USER@$ADDR:~/azure-measurements/$FILENAME ~/azure-measurements/results/$NAME"-results.csv" 
+	scp -i ~/.ssh/azure_key $USER@$ADDR:~/azure-measurements/$FILENAME ~/azure-measurements/results/$NAME"-results.csv"
+	cat $NAME"-results.csv" >> latency-results.csv
+	rm $NAME"-results.csv" 
 done
